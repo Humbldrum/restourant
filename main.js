183c11d8f3dbbd8d70eab0825d0699dac7224211
Humbldrum <113458263+Humbldrum@users.noreply.github.com>
2022-11-29 21:17:33 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+// Вывод в консоль
+
+
+
+
+// Переменные
+
+
+
+// Типы переменных?
+// let - прееменная
+
+
+
+// const - константа
+
+
+
+
+
+
+// типы данныыыых
+
+
+
+// const all = 'allo00000';
+// console.log (`ALLO ${all} wha`);
+
+
+
+
+// function all (name) {
+
+
+//     console.log (`ALLO ${name} wha`);
+
+
+// }
+
+//  all('yay') ;
+
+
+
+
+// function summ (a, b) {
+// const result = a+b
+
+
+// return result
+// }
+
+
+// let res = summ (11, 15);
+// console.log (res)
+
+
+
+
+
+
+
+
+// Массив
+
+// const there = ['first', 'second', 'other']
+
+
+
